5c94512e5d3adc85d146c8d60015cb71dab25071
vivekssharma07@gmail.com <sharma@Viveks-MacBook-Air.local>
2023-08-10 11:28:08 +0530

    Commit 2 feature

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crash", () => {
+it("renders without crash test ", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
